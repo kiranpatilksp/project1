cd5ed23a4ffcdd7af794238f5c26b8d83b17820a
kiranpatilksp <patilkiransatish881@gmail.com>
2022-04-29 19:27:29 +0530

    done

---
+++ jenkinsjava1.java
@@ -0,0 +1,8 @@
+class jenkinsjava1{
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("22Hello, World!");
+System.out.println("22Hello, 33World!");
+System.out.println("welcome");
+}
+}
